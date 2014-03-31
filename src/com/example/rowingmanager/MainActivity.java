00495b3fb9b24fbf3ecfd23921422281e62fc44b
Matthew Drabick <mdrabick@princeton.edu>
2014-03-31 00:32:07 -0400

    Working stopwatch. No recording of times. Displays elasped time in ten-thousandths upon stop, Pleaae enter the commit message for your changes. Lines starting

--- src/com/example/rowingmanager/MainActivity.java
+++ src/com/example/rowingmanager/MainActivity.java
@@ -1,15 +1,48 @@
 package com.example.rowingmanager;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends Activity {
-
+	
+	
+	private long startTime;
+	private Chronometer chrono;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		final TextView text = (TextView)findViewById(R.id.field);
+		final Button start_button=(Button)findViewById(R.id.start_button);
+		chrono = (Chronometer)findViewById(R.id.chronometer1);
+		start_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			   text.setText(getText(R.string.hello_text));
+			   startTime = System.currentTimeMillis();
+			   chrono.setBase(SystemClock.elapsedRealtime());
+			   chrono.start();
+			}
+	    });
+		final Button stop_button=(Button)findViewById(R.id.stop_button);
+		stop_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				long endTime = System.currentTimeMillis();
+				String elapsed = String.valueOf((endTime - startTime)/1000.0);
+				String elapsedSec = elapsed + " seconds";
+				text.setText(elapsedSec);
+				chrono.stop();
+			}
+	    });
 	}
 
 	@Override
@@ -19,7 +52,7 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	
+
 	
 
 }
